23e0fa9c545a2d5b80ba77fcf694d13d2f5652aa
mootil <mootility@gmail.com>
2023-03-28 20:09:01 +0900

    Add files via upload

---
+++ GithubTest.cpp
@@ -0,0 +1,13 @@
+﻿// GithubTest.cpp 
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "This is test for Github!\n";
+}
+
+// This is a code for github test.
